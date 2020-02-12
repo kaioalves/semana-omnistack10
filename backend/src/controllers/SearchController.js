488b3e821e0a77968cc64ca261458bea1003de29
kaioalves <kaio.alves7@hotmail.com>
2020-02-12 00:23:49 -0300

    Commit back-end e front-end

---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,28 @@
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+module.exports = {
+  async index(req, res) {
+    const { latitude, longitude, techs } = req.query;
+
+    const techsArray = parseStringAsArray(techs);
+
+    // filtrar todos os devs com as tecnologias XX próximos ao ponto (lat e lon) num raio de 10km
+    const devs = await Dev.find({
+      techs: {
+        $in: techsArray
+      },
+      location: {
+        $near: {
+          $geometry: {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000 //10km
+        }
+      }
+    });
+
+    return res.json({ devs });
+  }
+};
